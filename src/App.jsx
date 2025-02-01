8dc05b7dd27bf69d3eaa68571da7b8c071140b66
cristianCor19 <cordobac96@gmail.com>
2025-01-31 23:28:29 -0500

    feat: instance routes and context of the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,38 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import ProtectedRoute from "./components/routes/ProtectedRoute"
+import ProtectedLayout from "./components/routes/ProtectedLayout"
+
+//context 
+import { SessionProvider } from "./context/SessionContext"
+import { UserProvider } from "./context/UserContext"
 
 //pages
 import Login from "./pages/login"
 import RegisterUser from "./pages/RegisterUser"
+import HomePage from "./pages/HomePage"
+
 function App() {
  
 
   return (
     <BrowserRouter>
-      {/* <SessionProvider> */}
-        <div className="main-container">
-          <Routes>
-            <Route path="/" element={<Login/>}/>
-            <Route path="/register-user" element={<RegisterUser/>}/>
-          </Routes>
-        </div>
-      {/* </SessionProvider> */}
+      <SessionProvider>
+        <UserProvider>
+          <div className="main-container">
+            
+            <Routes>
+              <Route path="/" element={<Login/>}/>
+              <Route path="/register-user" element={<RegisterUser/>}/>
+              <Route element={<ProtectedRoute/>}>
+                <Route element={<ProtectedLayout/>}>
+                  <Route path="/home" element={<HomePage/> }/>
+                </Route>
+              </Route>
+            </Routes>
+          </div>
+        </UserProvider>
+      </SessionProvider>
       
     </BrowserRouter>
   )
