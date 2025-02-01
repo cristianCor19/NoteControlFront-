8ec0505a517b3d28d524494d4ddbb8395fad0757
cristianCor19 <cordobac96@gmail.com>
2025-01-31 23:27:40 -0500

    feat: implement forms of  sing in and register users with validations

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,90 @@
-function Login(){
+import { Link, useNavigate} from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useSession } from "../context/SessionContext";
+import { useEffect } from "react";
 
-    return (
-        <div>
-            <h2>Note Control</h2>
-            <p>Inicia sesión en tu cuenta</p>
-            <p></p>
-            <form action="">
-                <button>Iniciar sesión</button>
-            </form>
-        </div>
-    )
+
+import './../styles/login.css'
+
+function Login() {
+  const { register, handleSubmit, formState: { errors }, setValue,} = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const navigate = useNavigate();
+
+  const {signin ,isAuthenticated, errors: signinErrors} = useSession();
+
+
+  const onSubmit = handleSubmit(async(data) => {
+    try {
+      
+      await signin(data);
+      if (!isAuthenticated){
+        setValue('password', '');
+        
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  })
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/home")
+    }
+  }, [isAuthenticated, navigate]);
+
+  return (
+    <div className="flex items-center justify-center h-screen text-center">
+      <div>
+        <h2 className="text-4xl font-bold text-blue-ti">Note Control</h2>
+        <p className="mt-10 mb-4 font-bold">Inicia sesión en tu cuenta</p>
+        <p></p>
+        <form onSubmit={onSubmit} className="grid" >
+          <input type="email" placeholder="Digite su correo"
+          className="p-3 border-1 border-gray-50/100 mb-4 rounded-lg shadow-md w-70" 
+          {...register("email", {
+            required: "El correo es requerido",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "Correo inválido"
+            }
+          })}/>
+          {errors.email && (
+            <p className="text-red-500">{errors.email.message}</p>
+          )}
+          <input type="password" placeholder="Digite su contraseña"
+          className="p-3 border-1 border-gray-50/100 rounded-lg shadow-md" 
+          {...register("password", {
+            required: "La contraseña es requerida",
+            minLength: {
+              value: 8,
+              message: "La contraseña debe tener al menos 8 caracteres"
+            }
+          })}/>
+          {errors.password && (
+            <p className="text-red-500">{errors.password.message}</p>
+          )}
+          <button type="submit" className="bg-blue-ti text-white p-1.5 rounded-sm mt-6 mb-5">Iniciar sesión</button>
+        </form>
+        <Link to="/recovery-password" className="text-blue-link">
+          He olvidado mi contraseña
+        </Link>
+        <section className="pt-10">
+          <p className="pb-5">O continua con</p>
+          <div className="flex items-center justify-center gap-6">
+            <img src="/img/google-icon.svg" alt="" className="icons-login "/>
+            <img src="/img/facebook-icon.svg" alt="" className="icons-login shadow-amber-400"/>
+          </div>
+        </section>
+        <p className="mt-16">¿No tienes una cuenta? <Link to='/register-user' className="text-blue-ti font-bold">Registrate</Link></p>
+
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- src/pages/RegisterUser.jsx
+++ src/pages/RegisterUser.jsx
@@ -1,7 +1,136 @@
-function RegisterUser(){
-    return (
-        <div></div>
-    )
+import { Link, useNavigate} from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useUser } from "../context/UserContext";
+import { useEffect, useState} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+
+
+import './../styles/login.css'
+
+function RegisterUser() {
+  const { register, handleSubmit, formState: { errors }, setValue,} = useForm();
+//   const { register, handleSubmit, formState: { errors }, setValue,} = useForm({
+//     defaultValues: {
+//       email: "",
+//       password: "",
+//     },
+//   });
+  const navigate = useNavigate();
+  const {signUp ,registerUser,setRegisterUser, errors: signUpErrors} = useUser();
+  const [showPassword, setShowPassword] = useState(false);
+  const [alerPassword, setAlerPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(prevState => !prevState)
+  }
+
+
+  const onSubmit = handleSubmit(async(data) => {
+    try {
+        if(data.password !== data.confirmPassword){
+            console.log('The passwords are same');
+            setAlerPassword(true);
+        }else{
+            await signUp(data);
+            setAlerPassword(false);
+            console.log('are similars');
+            
+        }
+    
+    } catch (error) {
+      console.log(error.message);
+    }
+  })
+
+  useEffect(() => {
+    if (registerUser) {
+      navigate("/");
+      setRegisterUser(false);
+    }
+  }, [registerUser, navigate, setRegisterUser]);
+
+  return (
+    <div className="flex items-center justify-center h-screen text-center">
+      <div>
+        <h2 className="text-4xl font-bold text-blue-ti">Note Control</h2>
+        <p className="mt-10 mb-4 font-bold">Crea tu cuenta</p>
+        <p></p>
+        <form onSubmit={onSubmit} className="grid" >
+          <input type="email" placeholder="Digite su correo"
+          className="p-3 border-1 border-gray-50/100 mb-4 rounded-lg shadow-md w-70"
+          {...register("email", {
+            required: "El correo es requerido",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "Correo inválido"
+            }
+          })}/>
+          {errors.email && (
+            <p className="text-red-500">{errors.email.message}</p>
+          )}
+          <div className="relative">
+
+            <input type={showPassword ? "text" : "password"}  placeholder="Digite su contraseña"
+            className="p-3 border-1 border-gray-50/100 rounded-lg shadow-md mb-4 w-full"
+            {...register("password", {
+                required: "La contraseña es requerida",
+                minLength: {
+                value: 8,
+                message: "La contraseña debe tener al menos 8 caracteres"
+                }
+            })}/>
+            <button
+                type='button'
+                onClick={togglePasswordVisibility}
+                className='absolute top-1/2 right-2 transform -translate-y-1/2'
+                >
+                <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+            </button>
+          </div>
+            {errors.password && (
+                <p className="text-red-500">{errors.password.message}</p>
+            )}
+          <div className="relative">
+
+            <input type={showPassword ? "text" : "password"} placeholder="Confirma tu contraseña"
+            className="p-3 border-1 border-gray-50/100 rounded-lg shadow-md w-full"
+            {...register("confirmPassword", {
+                required: "La contraseña es requerida",
+                minLength: {
+                value: 8,
+                message: "La contraseña debe tener al menos 8 caracteres"
+                }
+            })}/>
+            <button
+                type='button'
+                onClick={togglePasswordVisibility}
+                className='absolute top-1/2 right-2 transform -translate-y-1/2 '
+                >
+                <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+            </button>
+          </div>
+          {errors.password && (
+            <p className="text-red-500">{errors.password.message}</p>
+          )}
+          {alerPassword && (
+            <p className="text-red-500">Las contraseña no coinciden</p>
+            
+          )}
+          <button type="submit" className="bg-blue-ti text-white p-1.5 rounded-sm mt-6 mb-5">Registrarte</button>
+        </form>
+        <section className="mt-6 mb-5">
+          <p className="pb-5">O continua con</p>
+          <div className="flex items-center justify-center gap-6">
+            <img src="/img/google-icon.svg" alt="" className="icons-login "/>
+            <img src="/img/facebook-icon.svg" alt="" className="icons-login shadow-amber-400"/>
+          </div>
+        </section>
+        <Link to='/' className="text-blue-ti font-bold">¿Ya tienes una cuenta?</Link>
+
+      </div>
+    </div>
+  );
 }
 
 export default RegisterUser;
