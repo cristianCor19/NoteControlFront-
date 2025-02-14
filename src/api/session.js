0ef466d74a00e29e2bf6947eb6ccd119d3ee9331
cristianCor19 <cordobac96@gmail.com>
2025-02-13 21:41:07 -0500

    feat: add reset password and email recovery functions to session API

--- src/api/session.js
+++ src/api/session.js
@@ -4,6 +4,9 @@ export const loginUserRequest = (user) => axios.post(`/session/signIn`, user);
 export const verifySessionRequest = (token) => axios.get(`/session/verify-session`, {
   headers: { 
     Authorization: `Bearer ${token}`
-
   }
 });
+
+export const resetPassword = (data) => axios.post(`/session/reset-password`, data)
+
+export const sendEmailRecovery = (email) => axios.post(`/session/send-email`, email);
